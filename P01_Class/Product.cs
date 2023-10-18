0e6358995b33786df95001ba238cb935314aa593
tee2490@hotmail.com <tee2490@hotmail.com>
2023-10-18 09:56:15 +0700

    Class & OOP

---
+++ P01_Class/Product.cs
@@ -0,0 +1,16 @@
+﻿
+namespace P01_Class
+{
+    public class Product  //แม่แบบ แบบฟอร์มกระดาษ
+    {
+        //attributes, property คุณสมบัติ
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+
+        //Default Method get set
+        //get อ่านค่า
+        //set ใส่ค่า
+    }
+}
---
+++ P01_Class/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using P01_Class;
+
+//Object วัตถุ
+Product product = new Product();
+Product ton = new Product();
+Product Sumo = new Product();
+
+product.Id = 1;
+product.Name = "Coffee";
+product.Price = 10;
+product.Amount = 20;
+
+ton.Id = 2;
+ton.Name = "Cream";
+ton.Price = 20;
+ton.Amount = 20;
+
+//Reuse ใช้ซ้ำ
+Display(product);
+Display(ton);
+
+void Display(Product p)
+{
+    Console.WriteLine($"{p.Id} {p.Name} {p.Price} {p.Amount}");
+}
